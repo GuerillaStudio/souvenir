a2a974096841cfefcc985ae1c955ed9b11016097
Tixie <admin@glitch.family>
2019-03-09 19:03:41 +0100

    fix: Load workbox locally instead of loading it by cdn (grr)

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,9 @@ module.exports = {
     themeColor: '#8420a7',
     msTileColor: '#212045',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black'
-
-    // // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    // workboxOptions: {
-    //   // swSrc is required in InjectManifest mode.
-    //   swSrc: 'dev/sw.js',
-    //   // ...other Workbox options...
-    // }
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      importWorkboxFrom: 'local'
+    }
   }
 }
