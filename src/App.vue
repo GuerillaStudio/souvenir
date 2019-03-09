1b90ac5b4aa7c2b7a997fd54d76ae263f0118843
Tixie <admin@glitch.family>
2019-03-09 02:02:18 +0100

    chore: Setup CSS skeleton

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="@/assets/css/style.css" lang="css"></style>
