161be7790988563f237974fb58a72e76574c98cf
Tixie <admin@glitch.family>
2019-03-09 02:23:48 +0100

    style: Setup to basic style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="txtcenter">Souvenir</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'souvenir',
 }
 </script>
 
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pwa: {
+    name: 'Souvenir',
+    themeColor: '#8420a7',
+    msTileColor: '#212045',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
+  }
+}
