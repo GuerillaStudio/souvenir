05bb7d30ca71aa3ae33f9af47055a0a94bb2d90a
wryk <wryk@wryk.moe>
2019-03-10 06:27:52 +0100

    fix crop

--- src/services/rectangle.js
+++ src/services/rectangle.js
@@ -9,7 +9,7 @@ export function makeRectangle (x, y, width, height) {
 
 export function crop ({ x, y, width: w, height: h }) {
   if (w < h) {
-    return makeRectangle((h - w) / 2, y, h, h)
+    return makeRectangle(x, (h - w) / 2, w, w)
   } else if (w > h) {
     return makeRectangle((w - h) / 2, y, h, h)
   } else {
