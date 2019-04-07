69ccbc780d26c1247899d3c3767264cb5e9f2255
Tixie <admin@glitch.family>
2019-04-07 02:47:14 +0200

    fix(router): Back on history mode because 👌

--- src/router.js
+++ src/router.js
@@ -9,11 +9,12 @@ import Download from '/views/screens/download'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  mode: 'hash',
+  mode: 'history',
   routes: [
     { name: 'home', path: '/', component: Welcome },
     { name: 'capture', path: '/capture', component: Capture },
     { name: 'preview', path: '/preview', component: Preview },
-    { name: 'download', path: '/download', component: Download }
+    { name: 'download', path: '/download', component: Download },
+    { path: '*', redirect: { name: 'home' } }
   ]
 })
