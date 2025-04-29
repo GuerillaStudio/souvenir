6f033efb4a7ba19970ca1d52e394a9eecf00451e
Tixie <contact@tixie.name>
2025-04-29 16:14:10 +0200

    Update source links in console.log

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ console.log(`%c
   └─┐│ ││ │└┐┌┘├┤ ││││├┬┘
   └─┘└─┘└─┘ └┘ └─┘┘└┘┴┴└─
 
-  Oh hi! If you're looking for the source code, It's here: https://github.com/GuerillaHQ/souvenir (or also here https://git.deuxfleurs.fr/GuerillaHQ/souvenir)
+  Oh hi! If you're looking for the source code, It's here: https://github.com/GuerillaStudio/souvenir (or also here https://git.deuxfleurs.fr/GuerillaStudio/souvenir)
 
   Have fun 💜
 
